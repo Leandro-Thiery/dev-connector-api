53b30b3cca675756d96817fa1c37e25a683150fc
Leandro-Thiery <leandrothiery1@gmail.com>
2022-01-04 23:12:32 +0700

    add database and query

---
+++ db.js
@@ -0,0 +1,9 @@
+const db = {
+    host: '13.76.97.86',
+    user: 'postgres',
+    port: 5432,
+    password: 'SparkGate2018',
+    database: 'postgres',
+};
+
+module.exports = {db};
--- server.js
+++ server.js
@@ -9,17 +9,23 @@ const createReadStream = require('fs').createReadStream;
 const sleep = require('util').promisify(setTimeout);
 const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
 const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
+const { db } = require('./db');
+const { Client } = require('pg');
 const app = express();
 
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('API Running');
+app.get('/', async (req, res) => {
+    const client = new Client(db);
+    await client.connect();
+    const selectAll = await client.query('select MAX(id) max from transactions;');
+    await client.end();
+    console.log(selectAll);
+    return res.send(selectAll.rows[0]);
 });
 
-app.post('/api/send-photo', async (req, res) => {
-    console.log(req.body);
+app.post('/api/sendPhoto', async (req, res) => {
     if (!req.body.image) {
         return res.status(400).json({
             status: 'Failed'
@@ -38,20 +44,70 @@ app.post('/api/send-photo', async (req, res) => {
     const printedTextSampleURL = req.body.image;
 
     var input = Buffer.from(printedTextSampleURL, 'base64');
-    console.log(input);
-    // Status strings returned from Read API. NOTE: CASING IS SIGNIFICANT.
-    // Before Read 3.0, these are "Succeeded" and "Failed"
 
     // Recognize text in printed image from a URL
     console.log('Read printed text from URL...', printedTextSampleURL.split('/').pop());
     const printedResult = await readTextFromURL(computerVisionClient, input);
     let output = printRecText(printedResult);
-
     const match = output.match(/[A-Z]{1,2}\s{1}\d{1,4}\s{1}[A-Z]{1,3}/);
-    res.json({
-        status: 'OKE',
-        text: match[0]
-    });
+    if (match.length == 0) {
+        return res.status(404).json({
+            status: 'Failed'
+        });
+    }
+    const licensePlate = match[0];
+
+    try {
+        // Insert INTO Database
+        const client = new Client(db);
+        await client.connect();
+        const carUserId = await client.query(`SELECT userid FROM car WHERE platenumber ILIKE $1`, [licensePlate]);
+        if (carUserId.rowCount == 0) {
+            return res.status(404).json({
+                status: 'Failed, car not recognised'
+            });
+        }
+        const userId = carUserId.rows[0].userid;
+
+        const time = new Date();
+        const year = time.getFullYear();
+        const month = time.getMonth() + 1;
+        const day = time.getDate();
+        const hour = time.getHours();
+        const minute = time.getMinutes();
+
+        const transaction = await client.query(`SELECT id, status FROM transactions WHERE platenumber ILIKE $1 ORDER BY id DESC LIMIT 1`, [licensePlate]);
+        if (transaction.rows[0].id != null && (transaction.rows[0].status == 'Inside')) {
+                // UPDATE OUTSIDE
+                const transId = transaction.rows[0].id;
+                const updateTrans = await client.query(`UPDATE transactions SET yearout = $1, 
+                    monthout = $2, dayout = $3, hourout = $4, 
+                    minout = $5 , status = 'Outside' WHERE id = $6`, [
+                    year, month, day, hour, minute, transId
+                ]);
+        } else {
+            const getMaxId = await client.query(`SELECT MAX(id) max FROM transactions`);
+            const newId = getMaxId.rows[0].max + 1;
+
+            await client.query(`INSERT INTO transactions 
+        (id, userid, platenumber, yearin, monthin, dayin, hourin, minin, status) 
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`, [
+                newId, userId, licensePlate, year, month, day, hour, minute, 'Inside'
+            ]);
+        }
+        await client.end();
+        res.json({
+            status: 'OKE',
+            text: licensePlate,
+            time: time.toISOString()
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            status: 'Failed'
+        });
+    }
+
 });
 
 
@@ -78,7 +134,7 @@ function printRecText(readResults) {
         if (result.lines.length) {
             for (const line of result.lines) {
                 text += (line.words.map(w => w.text).join(' ')) + '\n';
-                console.log(line.words.map(w => w.text).join(' '))
+                console.log(line.words.map(w => w.text).join(' '));
             }
         }
         else { console.log('No recognized text.'); }
